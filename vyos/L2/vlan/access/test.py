802a148c9dbe980a31a9e650ed60d1baed1ddbc9
knknkn1162 <knknkn1162@gmail.com>
2024-07-26 15:32:33 +0900

    access port

---
+++ vyos/L2/vlan/access/test.py
@@ -0,0 +1,71 @@
+from genie import testbed
+from cmlmag.cml import CONFIG_YAML, Cml
+from cmlmag.device import Device
+from cmlmag import wait, ipv4
+import cmlmag.parse as parse
+import cmlmag.wait_until as wait_until
+import ini
+from cmlmag.structure.stp_info import (
+  Role as StpRole,
+  State as StpState
+)
+
+def main():
+
+  tb = testbed.load(CONFIG_YAML)
+  # switch
+  vyos0 = Device(tb, 'vyos0')
+  pc1 = Device(tb, 'pc1')
+  pc2 = Device(tb, 'pc2')
+  pc3 = Device(tb, 'pc3')
+  pc4 = Device(tb, 'pc4')
+
+  # ip
+  pc1.execs([
+    f"ip {ini.pc1.eth0.ip_addr.ip} {ini.pc1.eth0.ip_addr.netmask}",
+    f"show",
+  ])
+
+  pc2.execs([
+    f"ip {ini.pc2.eth0.ip_addr.ip} {ini.pc2.eth0.ip_addr.netmask}",
+    f"show",
+  ])
+
+  pc3.execs([
+    f"ip {ini.pc3.eth0.ip_addr.ip} {ini.pc3.eth0.ip_addr.netmask}",
+    f"show",
+  ])
+
+  pc4.execs([
+    f"ip {ini.pc4.eth0.ip_addr.ip} {ini.pc4.eth0.ip_addr.netmask}",
+    f"show",
+  ])
+
+  wait_until.seconds(5)
+  # it should not work.
+  pc1.vpcs_ping(ini.pc3.eth0.ip_addr.ip)
+  pc1.vpcs_ping(ini.pc2.eth0.ip_addr.ip)
+  pc2.vpcs_ping(ini.pc4.eth0.ip_addr.ip)
+
+
+  vyos0.vyos_execs([
+    [
+      f"set interfaces bridge br0",
+      f"set interfaces bridge br0 enable-vlan",
+      # associate vlan to eth (access port)
+      f"set interfaces bridge br0 member interface eth0 native-vlan {ini.vyos0.br0.eth0.vlan_num}",
+      f"set interfaces bridge br0 member interface eth1 native-vlan {ini.vyos0.br0.eth1.vlan_num}",
+      f"set interfaces bridge br0 member interface eth2 native-vlan {ini.vyos0.br0.eth2.vlan_num}",
+      f"set interfaces bridge br0 member interface eth3 native-vlan {ini.vyos0.br0.eth3.vlan_num}",
+    ],
+  ])
+
+  wait_until.seconds(5)
+  # it should work!
+  pc1.vpcs_ping(ini.pc3.eth0.ip_addr.ip)
+  # it should not work.
+  pc1.vpcs_ping(ini.pc2.eth0.ip_addr.ip)
+  pc2.vpcs_ping(ini.pc4.eth0.ip_addr.ip)
+
+if __name__ == '__main__':
+  main()
