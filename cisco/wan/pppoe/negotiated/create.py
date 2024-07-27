96e11a807a376e3e9ec788d1234ce448b652e9e8
knknkn1162 <knknkn1162@gmail.com>
2024-07-27 21:24:26 +0900

    pppoe enable in client

--- cisco/wan/pppoe/negotiated/create.py
+++ cisco/wan/pppoe/negotiated/create.py
@@ -9,10 +9,10 @@ def main():
     r1 = lab.create_iosv(ini.iosv_1.__name__, 700, 700, slots=1)
     r2 = lab.create_iosv(ini.iosv_2.__name__, 1000, 400, slots=2)
 
-    c0 = lab.create_server(ini.server_1.__name__, 1000, 700, slots=1)
+    #c0 = lab.create_server(ini.server_1.__name__, 1000, 700, slots=1)
 
-    c0.create_links([r2[1]])
-    r2.create_links([r1[0], c0[0]])
+    #c0.create_links([r2[1]])
+    r2.create_links([r1[0]])
     r1.create_links([r2[0]])
     
     lab.start()
--- cisco/wan/pppoe/negotiated/test.py
+++ cisco/wan/pppoe/negotiated/test.py
@@ -13,8 +13,6 @@ def main():
   iosv_1 = Device(tb, 'iosv_1')
   iosv_2 = Device(tb, 'iosv_2')
 
-  server_1 = Device(tb, 'server_1')
-
   # server
   iosv_1.execs([
     [
@@ -35,6 +33,7 @@ def main():
     # virtual template settings
     [
       f"interface {ini.iosv_1.vt1.name}",
+      # use loopback0 ip address
       f"ip unnumbered {ini.iosv_1.loopback0.name}",
       f"peer default ip address pool {ini.pool_name}",
     ],
@@ -48,8 +47,10 @@ def main():
   iosv_2.execs([
     [
       f"interface {ini.iosv_2.g0_0.name}",
+      f"pppoe enable",
       # dialer pool 1に所属するインターフェイスはpppoeを使って接続するように設定
       f"pppoe-client dial-pool-number {ini.iosv_2.dialer0.pool_num}",
+      f"no ip address",
       f"no shutdown",
     ],
     # dialer settings
@@ -69,6 +70,9 @@ def main():
     ],
   ])
 
+  iosv_1.show_mac_ip()
+  iosv_2.show_mac_ip()
+
   def populate_router_ping(device: Device, target_ip: str, sleep_time=3):
     @wait.retry(count=30, result=0, sleep_time=sleep_time)
     def _populate_router_ping(device: Device):
@@ -82,6 +86,7 @@ def main():
   populate_router_ping(iosv_2, ini.iosv_1.loopback0.ip_addr.ip)
 
   iosv_1.execs([
+    # check my MAC address and peer are shown
     f"show pppoe session",
     f"show pppoe session all",
     f"show ip interface {ini.iosv_1.vt1.access_name}",
@@ -90,6 +95,7 @@ def main():
   ])
 
   iosv_2.execs([
+    # check my MAC address and peer are shown
     f"show pppoe session",
     f"show pppoe session all",
     f"show interface {ini.iosv_2.dialer0.name}",
@@ -97,42 +103,5 @@ def main():
     f"show ip route",
   ])
 
-  # PAT settings
-  server_1.execs([
-
-    f"[ -f /var/run/udhcpc.eth0.pid ] && sudo kill `cat /var/run/udhcpc.eth0.pid`",
-    f"sudo ifconfig eth0 {ini.server_1.eth0.ip_addr.ip} netmask {ini.server_1.eth0.ip_addr.netmask} up",
-    f"sudo route add default gw {ini.iosv_2.g0_1.ip_addr.ip}",
-    f"ifconfig eth0",
-    f"route -e",
-  ])
-
-  iosv_2.execs([
-    [
-      f"interface {ini.iosv_2.g0_1.name}",
-      f"ip address {ini.iosv_2.g0_1.ip_addr.ip} {ini.iosv_2.g0_1.ip_addr.netmask}",
-      f"ip nat inside",
-      f"no shutdown",
-    ],
-    [
-      f"interface {ini.iosv_2.dialer0.name}",
-      f"ip nat outside",
-    ],
-    [
-      f"ip route 0.0.0.0 0.0.0.0 {ini.iosv_2.dialer0.name}",
-    ],
-    # PAT settings
-    [
-      f"access-list {ini.acl_num} permit {ini.iosv_2.g0_1.ip_addr.network.network_address} {ini.iosv_2.g0_1.ip_addr.hostmask}",
-      f"ip nat inside source list {ini.acl_num} interface {ini.iosv_2.dialer0.name} overload",
-    ]
-  ])
-
-  wait.seconds(30)
-  server_1.server_ping(ini.iosv_1.loopback0.ip_addr.ip)
-  iosv_2.execs([
-    f"show ip nat translations",
-  ])
-  
 if __name__ == '__main__':
   main()
