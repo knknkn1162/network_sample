81dd099154f2685f76345d60aad28bc717399e59
knknkn1162 <knknkn1162@gmail.com>
2024-07-29 15:45:49 +0900

    VRF-life ospf version

---
+++ cisco/routing/vrf/vrf_lite/ospf/ini.py
@@ -0,0 +1,70 @@
+from ipaddress import ip_interface
+
+pcap_file = "test.pcap"
+vc_id = 1
+
+class site_a:
+   name = "SITE-A"
+   rd = "10:10"
+   ospf_process_id = 1
+
+class site_b:
+   name = "SITE-B"
+   rd = "20:20"
+   ospf_process_id = 2
+
+class iosv_0:
+   class g0_0:
+      name = "GigabitEthernet0/0"
+      ip_addr = ip_interface("192.168.0.254/24")
+      site = site_a
+   class g0_1:
+      name = "GigabitEthernet0/1"
+      ip_addr = ip_interface("192.168.0.254/24")
+      site = site_b
+   class g0_2:
+      name = "GigabitEthernet0/2"
+      class sub0:
+         num = 1
+         ip_addr = ip_interface("192.168.10.1/24")
+         site = site_a
+      class sub1:
+         num = 2
+         ip_addr = ip_interface("192.168.10.1/24")
+         site = site_b
+
+class iosv_1:
+   class g0_0:
+      name = "GigabitEthernet0/0"
+      ip_addr = ip_interface("192.168.1.254/24")
+      site = site_a
+   class g0_1:
+      name = "GigabitEthernet0/1"
+      ip_addr = ip_interface("192.168.1.254/24")
+      site = site_b
+   class g0_2:
+      name = "GigabitEthernet0/2"
+      class sub0:
+         num = 1
+         ip_addr = ip_interface("192.168.10.2/24")
+         site = site_a
+      class sub1:
+         num = 2
+         ip_addr = ip_interface("192.168.10.2/24")
+         site = site_b
+
+class server_0:
+    class eth0:
+      ip_addr = ip_interface("192.168.0.1/24")
+
+class server_1:
+    class eth0:
+      ip_addr = ip_interface("192.168.0.2/24")
+
+class server_2:
+    class eth0:
+      ip_addr = ip_interface("192.168.1.1/24")
+
+class server_3:
+    class eth0:
+      ip_addr = ip_interface("192.168.1.2/24")
---
+++ cisco/routing/vrf/vrf_lite/ospf/test.py
@@ -0,0 +1,241 @@
+from genie import testbed
+from cmlmag.cml import CONFIG_YAML, Cml
+from cmlmag.device import Device
+from cmlmag import wait, ipv4
+import cmlmag.parse as parse
+import cmlmag.wait_until as wait_until
+import ini
+from cmlmag.structure.stp_info import (
+  Role as StpRole,
+  State as StpState
+)
+
+def main():
+  tb = testbed.load(CONFIG_YAML)
+  # switch
+  iosv_0 = Device(tb, ini.iosv_0.__name__)
+  iosv_1 = Device(tb, ini.iosv_1.__name__)
+
+  server_0 = Device(tb, ini.server_0.__name__)
+  server_1 = Device(tb, ini.server_1.__name__)
+  server_2 = Device(tb, ini.server_2.__name__)
+  server_3 = Device(tb, ini.server_3.__name__)
+  print("####### exec #######")
+
+  cml = Cml()
+  pcap = cml.lab.create_pcap(iosv_0.name, iosv_1.name, auth_token=cml.auth_token)
+
+  # server setup
+  server_0.execs([
+    # eth0 setting
+    ## disable DHCP
+    f"[ -f /var/run/udhcpc.eth0.pid ] && sudo kill `cat /var/run/udhcpc.eth0.pid`",
+    f"sudo ifconfig eth0 {ini.server_0.eth0.ip_addr.ip} netmask {ini.server_0.eth0.ip_addr.netmask} up",
+    f"sudo route add default gw {ini.iosv_0.g0_0.ip_addr.ip}",
+    f"ifconfig eth0",
+    f"route -e",
+  ])
+
+  server_1.execs([
+    # eth0 setting
+    ## disable DHCP
+    f"[ -f /var/run/udhcpc.eth0.pid ] && sudo kill `cat /var/run/udhcpc.eth0.pid`",
+    f"sudo ifconfig eth0 {ini.server_1.eth0.ip_addr.ip} netmask {ini.server_1.eth0.ip_addr.netmask} up",
+    f"sudo route add default gw {ini.iosv_0.g0_1.ip_addr.ip}",
+    f"ifconfig eth0",
+    f"route -e",
+  ])
+
+  server_2.execs([
+    # eth0 setting
+    ## disable DHCP
+    f"[ -f /var/run/udhcpc.eth0.pid ] && sudo kill `cat /var/run/udhcpc.eth0.pid`",
+    f"sudo ifconfig eth0 {ini.server_2.eth0.ip_addr.ip} netmask {ini.server_2.eth0.ip_addr.netmask} up",
+    f"sudo route add default gw {ini.iosv_1.g0_0.ip_addr.ip}",
+    f"ifconfig eth0",
+    f"route -e",
+  ])
+
+  server_3.execs([
+    # eth0 setting
+    ## disable DHCP
+    f"[ -f /var/run/udhcpc.eth0.pid ] && sudo kill `cat /var/run/udhcpc.eth0.pid`",
+    f"sudo ifconfig eth0 {ini.server_3.eth0.ip_addr.ip} netmask {ini.server_3.eth0.ip_addr.netmask} up",
+    f"sudo route add default gw {ini.iosv_1.g0_1.ip_addr.ip}",
+    f"ifconfig eth0",
+    f"route -e",
+  ])
+
+  # vrf setting first
+  # VRF-lite setting
+  iosv_0.execs([
+    [
+      f"ip vrf {ini.site_a.name}",
+      f"rd {ini.site_a.rd}",
+    ],
+    [
+      f"ip vrf {ini.site_b.name}",
+      f"rd {ini.site_b.rd}",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_0.name}",
+      f"ip vrf forwarding {ini.iosv_0.g0_0.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_1.name}",
+      f"ip vrf forwarding {ini.iosv_0.g0_1.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}.{ini.iosv_0.g0_2.sub0.num}",
+      f"ip vrf forwarding {ini.iosv_0.g0_2.sub0.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}.{ini.iosv_0.g0_2.sub1.num}",
+      f"ip vrf forwarding {ini.iosv_0.g0_2.sub1.site.name}",
+    ],
+  ])
+
+  iosv_1.execs([
+    [
+      f"ip vrf {ini.site_a.name}",
+      f"rd {ini.site_a.rd}",
+    ],
+    [
+      f"ip vrf {ini.site_b.name}",
+      f"rd {ini.site_b.rd}",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_0.name}",
+      f"ip vrf forwarding {ini.iosv_1.g0_0.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_1.name}",
+      f"ip vrf forwarding {ini.iosv_1.g0_1.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_2.name}.{ini.iosv_1.g0_2.sub0.num}",
+      f"ip vrf forwarding {ini.iosv_1.g0_2.sub0.site.name}",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_2.name}.{ini.iosv_1.g0_2.sub1.num}",
+      f"ip vrf forwarding {ini.iosv_1.g0_2.sub1.site.name}",
+    ],
+  ])
+
+  ## up
+  iosv_0.execs([
+    [
+      f"interface {ini.iosv_0.g0_0.name}",
+      f"ip addr {ini.iosv_0.g0_0.ip_addr.ip} {ini.iosv_0.g0_0.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_1.name}",
+      # if vrf setting later, <ip_addr> overlaps with ..
+      f"ip addr {ini.iosv_0.g0_1.ip_addr.ip} {ini.iosv_0.g0_1.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}",
+      f"no ip address",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}.{ini.iosv_0.g0_2.sub0.num}",
+      # Configuring IP routing on a LAN subinterface is only allowed if that 
+      # subinterface is already configured as part of an IEEE 802.10, IEEE 802.1Q, 
+      # or ISL vLAN
+      f"encapsulation dot1Q {ini.iosv_0.g0_2.sub0.num}",
+      f"ip addr {ini.iosv_0.g0_2.sub0.ip_addr.ip} {ini.iosv_0.g0_2.sub0.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}.{ini.iosv_0.g0_2.sub1.num}",
+      f"encapsulation dot1Q {ini.iosv_0.g0_2.sub1.num}",
+      f"ip addr {ini.iosv_0.g0_2.sub1.ip_addr.ip} {ini.iosv_0.g0_2.sub1.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+  ])
+
+  iosv_1.execs([
+    [
+      f"interface {ini.iosv_1.g0_0.name}",
+      f"ip addr {ini.iosv_1.g0_0.ip_addr.ip} {ini.iosv_1.g0_0.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_1.name}",
+      f"ip addr {ini.iosv_1.g0_1.ip_addr.ip} {ini.iosv_1.g0_1.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_0.g0_2.name}",
+      f"no ip address",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_2.name}.{ini.iosv_0.g0_2.sub0.num}",
+      f"encapsulation dot1Q {ini.iosv_1.g0_2.sub0.num}",
+      f"ip addr {ini.iosv_1.g0_2.sub0.ip_addr.ip} {ini.iosv_1.g0_2.sub0.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+    [
+      f"interface {ini.iosv_1.g0_2.name}.{ini.iosv_1.g0_2.sub1.num}",
+      f"encapsulation dot1Q {ini.iosv_1.g0_2.sub1.num}",
+      f"ip addr {ini.iosv_1.g0_2.sub1.ip_addr.ip} {ini.iosv_1.g0_2.sub1.ip_addr.netmask}",
+      f"no shutdown",
+    ],
+  ])
+
+  # routing(OSPF)
+  iosv_0.execs([
+    [
+      f"router ospf {ini.site_a.ospf_process_id} vrf {ini.site_a.name}",
+      f"network {ini.iosv_0.g0_0.ip_addr.network.network_address} {ini.iosv_0.g0_0.ip_addr.hostmask} area 0",
+      f"network {ini.iosv_0.g0_2.sub0.ip_addr.network.network_address} {ini.iosv_0.g0_2.sub0.ip_addr.hostmask} area 0",
+    ],
+    [
+      f"router ospf {ini.site_b.ospf_process_id} vrf {ini.site_b.name}",
+      f"network {ini.iosv_0.g0_1.ip_addr.network.network_address} {ini.iosv_0.g0_1.ip_addr.hostmask} area 0",
+      f"network {ini.iosv_0.g0_2.sub1.ip_addr.network.network_address} {ini.iosv_0.g0_2.sub1.ip_addr.hostmask} area 0",
+    ]
+  ])
+
+  iosv_1.execs([
+    [
+      f"router ospf {ini.site_a.ospf_process_id} vrf {ini.site_a.name}",
+      f"network {ini.iosv_1.g0_0.ip_addr.network.network_address} {ini.iosv_1.g0_0.ip_addr.hostmask} area 0",
+      f"network {ini.iosv_1.g0_2.sub0.ip_addr.network.network_address} {ini.iosv_1.g0_2.sub0.ip_addr.hostmask} area 0",
+    ],
+    [
+      f"router ospf {ini.site_b.ospf_process_id} vrf {ini.site_b.name}",
+      f"network {ini.iosv_1.g0_1.ip_addr.network.network_address} {ini.iosv_1.g0_1.ip_addr.hostmask} area 0",
+      f"network {ini.iosv_1.g0_2.sub1.ip_addr.network.network_address} {ini.iosv_1.g0_2.sub1.ip_addr.hostmask} area 0",
+    ]
+  ])
+
+  wait.seconds(50)
+  pcap.start(maxpackets=100)
+  server_0.server_ping(ini.server_2.eth0.ip_addr.ip)
+  server_1.server_ping(ini.server_3.eth0.ip_addr.ip)
+  pcap.download(file=ini.pcap_file)
+
+  iosv_0.execs([
+    f"show ip vrf brief",
+    f"show ip route",
+    f"show ip route vrf {ini.site_a.name}",
+    f"show ip route vrf {ini.site_b.name}",
+  ])
+  iosv_1.execs([
+    f"show ip vrf brief",
+    f"show ip route",
+    f"show ip route vrf {ini.site_a.name}",
+    f"show ip route vrf {ini.site_b.name}",
+  ])
+  
+  # pcap.start(maxpackets=500)
+  # server_0.server_ping(ini.server_1.eth0.ip_addr.ip)
+  # pcap.download(file=ini.pcap_file)
+
+if __name__ == '__main__':
+  main()
